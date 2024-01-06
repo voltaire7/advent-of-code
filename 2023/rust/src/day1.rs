127f74bdd490f95afd75629b0cfc67e9c4310533
Ioan Mark Neaga <1932951@cegepmontpetit.ca>
2024-01-06 14:16:40 -0500

    Finished part 2
    There's a bug somewhere. Don't where exactly.

--- 2023/rust/src/day1.rs
+++ 2023/rust/src/day1.rs
@@ -25,8 +25,6 @@
 //     dbg!(content);
 // }
 
-#![allow(dead_code)]
-
 use std::fs;
 
 pub fn part1() {
@@ -59,19 +57,96 @@ pub fn part1() {
 pub fn part2() {
     // Read entire file
     let content = fs::read_to_string("../inputs/day-1").unwrap();
-    let mut chars = content.chars();
+    let content: Vec<char> = content.chars().collect();
+    let mut new: Vec<char> = Vec::new();
 
-    // Get digits
-    let mut new_content = String::new();
-    loop {
-        let c = chars.next();
-        if c == None {
-            break;
+    let mut i = 0;
+    while i < content.len() {
+        let c = content[i];
+
+        if c.is_ascii_digit() || c == '\n' {
+            new.push(content[i]);
+        } else {
+            match c {
+                'o' => {
+                    if content[i + 1] == 'n' && content[i + 2] == 'e' {
+                        new.push('1');
+                        i += 2;
+                    }
+                }
+                't' => {
+                    if content[i + 1] == 'w' && content[i + 2] == 'o' {
+                        new.push('2');
+                        i += 2;
+                    } else if content[i + 1] == 'h'
+                        && content[i + 2] == 'r'
+                        && content[i + 3] == 'e'
+                        && content[i + 4] == 'e'
+                    {
+                        new.push('3');
+                        i += 4;
+                    }
+                }
+                'f' => {
+                    if content[i + 1] == 'o' && content[i + 2] == 'u' && content[i + 3] == 'r' {
+                        new.push('4');
+                        i += 3;
+                    } else if content[i + 1] == 'i'
+                        && content[i + 2] == 'v'
+                        && content[i + 3] == 'e'
+                    {
+                        new.push('5');
+                        i += 3;
+                    }
+                }
+                's' => {
+                    if content[i + 1] == 'i' && content[i + 2] == 'x' {
+                        new.push('6');
+                        i += 2;
+                    } else if content[i + 1] == 'e'
+                        && content[i + 2] == 'v'
+                        && content[i + 3] == 'e'
+                        && content[i + 4] == 'n'
+                    {
+                        new.push('7');
+                        i += 4;
+                    }
+                }
+                'e' => {
+                    if content[i + 1] == 'i'
+                        && content[i + 2] == 'g'
+                        && content[i + 3] == 'h'
+                        && content[i + 4] == 't'
+                    {
+                        new.push('8');
+                        i += 4;
+                    }
+                }
+                'n' => {
+                    if content[i + 1] == 'i' && content[i + 2] == 'n' && content[i + 3] == 'e' {
+                        new.push('9');
+                        i += 3;
+                    }
+                }
+                _ => (),
+            }
         }
-        let c = c.unwrap();
 
-        new_content.push(c);
+        i += 1;
     }
+    let content: String = new.iter().collect();
 
-    dbg!(new_content);
+    let content: Vec<String> = content
+        .split_whitespace()
+        .map(|s| {
+            let first = s.chars().next().unwrap().to_string();
+            let last = s.chars().last().unwrap().to_string();
+            return first + &last;
+        })
+        .collect();
+
+    // Parse to number
+    let content: usize = content.iter().map(|s| s.parse::<usize>().unwrap()).sum();
+
+    dbg!(content);
 }
--- 2023/rust/src/main.rs
+++ 2023/rust/src/main.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 mod day1;
 
 fn main() {
