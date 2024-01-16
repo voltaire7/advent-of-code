0a6bca838ab203c8eb131d55dfa96d4c0086070f
Ioan Mark Neaga <1932951@cegepmontpetit.ca>
2024-01-16 02:06:09 -0500

    Removed unneeded project (again??)

---
+++ 2023/rust/src/day2.rs
@@ -0,0 +1,6 @@
+use core::str;
+
+pub fn part1() {
+    let content: Vec<&str> = include_str!("../../inputs/day-2").split('\n').collect();
+    dbg!(content);
+}
--- 2023/rust/src/main.rs
+++ 2023/rust/src/main.rs
@@ -1,7 +1,18 @@
-#![allow(dead_code)]
+use std::env;
 
 mod day1;
+mod day2;
 
 fn main() {
-    day1::part2();
+    if env::args().len() != 2 {
+        println!("[ERROR] Invalid number of arguments. Please use this format: cargo run 1a");
+        return;
+    }
+    let id = env::args().nth(1).unwrap();
+    match id.as_str() {
+        "1a" => day1::part1(),
+        "1b" => day1::part2(),
+        "2a" => day2::part1(),
+        _ => println!("[ERROR] Project with id '{id}' does not exist."),
+    }
 }
