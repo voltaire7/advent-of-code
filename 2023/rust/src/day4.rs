327883193683be64a7be0c823a2ec1e01b28d742
Ioan Mark Neaga <1932951@cegepmontpetit.ca>
2024-02-21 08:31:06 -0500

    4b

--- 2023/rust/src/day4.rs
+++ 2023/rust/src/day4.rs
@@ -38,5 +38,43 @@ pub fn part1() {
 }
 
 pub fn part2() {
-    todo!()
+    let content = include_str!("../../inputs/day-4")
+        .trim()
+        .split('\n')
+        .collect::<Vec<&str>>();
+    let mut content = content
+        .into_iter()
+        .map(|x| {
+            let x = x.split(": ").last().unwrap();
+            let mut x = x.split(" | ");
+            let w = x.next().unwrap();
+            let a = x.next().unwrap();
+            let parse = |y: &str| {
+                y.split_whitespace()
+                    .filter_map(|y| {
+                        if y == " " {
+                            None
+                        } else {
+                            Some(y.parse::<u32>().unwrap())
+                        }
+                    })
+                    .collect::<Vec<u32>>()
+            };
+            let (w, a) = (parse(w), parse(a));
+            (
+                w.into_iter()
+                    .filter(|y| a.clone().into_iter().any(|z| z == *y))
+                    .count(),
+                1,
+            )
+        })
+        .collect::<Vec<(usize, usize)>>();
+    (0..content.len()).for_each(|x| {
+        if content[x].0 != 0 {
+            (x + 1..x + 1 + content[x].0).for_each(|y| {
+                content[y].1 += 1 * content[x].1;
+            });
+        }
+    });
+    dbg!(content.into_iter().map(|x| x.1).sum::<usize>());
 }
