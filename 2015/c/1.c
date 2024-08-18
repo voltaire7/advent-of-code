b8ebbcbc0ea0930f50cf67a5eef7db4653ef6fd6
Ioan Mark Neaga <1932951@cegepmontpetit.ca>
2024-08-17 21:28:07 -0400

    [C] day 1, part 1

--- 2015/c/1.c
+++ 2015/c/1.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    printf("Hello, World!\n");
+    FILE* file = fopen("./inputs/day-1", "r");
+    fseek(file, 0, SEEK_END);
+    long fsize = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    char* str = malloc(fsize + 1);
+    fread(str, fsize, 1, file);
+    fclose(file);
+
+    str[fsize] = 0;
+
+    int floor = 0;
+    for (int i = 0; i < fsize; i++)
+        if (str[i] == '(')
+            floor++;
+        else if (str[i] == ')')
+            floor--;
+    printf("%i\n", floor);
+
+    free(str);
 }
