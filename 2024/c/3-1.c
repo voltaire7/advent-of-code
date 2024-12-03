ec6d846f407742faea1770ecdb1d00414cfc838d
Ioan Mark Neaga <1932951@cegepmontpetit.ca>
2024-12-03 01:00:03 -0500

    2024/c/3-1

---
+++ 2024/c/3-1.c
@@ -0,0 +1,56 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <regex.h>
+
+typedef struct Pair {
+    int left;
+    int right;
+} Pair;
+
+char* read_entire_file(char* filename) {
+    FILE* file = fopen(filename, "r");
+    fseek(file, 0, SEEK_END);
+    long fsize = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    char* str = malloc(fsize + 1);
+    fread(str, fsize, 1, file);
+    fclose(file);
+
+    str[fsize] = 0;
+    return str;
+}
+
+Pair get_pair(char **content) {
+    int left, right;
+    for (; *(*content + 4); ++*content) {
+        if (strncmp("mul(", *content, 4) != 0) continue;
+        char *temp = *content;
+        *content += 4;
+
+        left = atoi(*content);
+        for (; isdigit(**content); ++*content);
+
+        if (**content != ',') continue;
+
+        right = atoi(++*content);
+        for (; isdigit(**content); ++*content);
+
+        if (**content != ')') continue;
+        else return (Pair){left, right};
+    }
+    return (Pair){};
+}
+
+int main() {
+    char *content = read_entire_file("inputs/day-3");
+    int sum = 0;
+    for (;;) {
+        Pair pair = get_pair(&content);
+        if (pair.left != 0 && pair.right != 0) sum += pair.left * pair.right;
+        else break;
+    }
+    printf("sum: %i\n", sum);
+}
