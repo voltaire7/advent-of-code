3caa02bad6efda974d38d32e6c01b12a2791fc2f
Ioan Mark Neaga <1932951@cegepmontpetit.ca>
2024-12-08 01:32:58 -0500

    Removed debug stuff

--- 2024/c/8-1.c
+++ 2024/c/8-1.c
@@ -11,7 +11,6 @@ void find(int *y, int *x) {
     char c = map[*y][*x];
     for (int i = *y; i < SIZE; i++) {
         for (int j = i > *y ? 0 : *x + 1; j < SIZE; j++) {
-            // printf("debug 3: %c, %i, %i\n", c, i, j);
             if (map[i][j] == c) {
                 *y = i, *x = j;
                 return;
@@ -32,8 +31,6 @@ int main() {
             int y2 = y1, x2 = x1;
             for (;;) {
                 find(&y2, &x2);
-                // printf("debug 1: %c, %i, %i\n", map[y1][x1], y1, x1);
-                // printf("debug 2: %c, %i, %i\n\n", map[y2][x2], y2, x2);
                 if (y2 < 0 || x2 < 0) break;
                 int disty = y2 - y1, distx = x2 - x1;
                 if (y1-disty >= 0 && y1-disty < SIZE && x1-distx >= 0 && x1-distx < SIZE) bools[y1-disty][x1-distx] = true;
@@ -49,9 +46,4 @@ int main() {
         }
     }
     printf("count: %i\n", count);
-
-    return 0;
-    for (int i = 0; i < SIZE; i++) {
-        printf("%.*s\n", SIZE, map[i]);
-    }
 }
--- 2024/c/8-2.c
+++ 2024/c/8-2.c
@@ -11,7 +11,6 @@ void find(int *y, int *x) {
     char c = map[*y][*x];
     for (int i = *y; i < SIZE; i++) {
         for (int j = i > *y ? 0 : *x + 1; j < SIZE; j++) {
-            // printf("debug 3: %c, %i, %i\n", c, i, j);
             if (map[i][j] == c) {
                 *y = i, *x = j;
                 return;
@@ -33,8 +32,6 @@ int main() {
             int y2 = y1, x2 = x1;
             for (;;) {
                 find(&y2, &x2);
-                // printf("debug 1: %c, %i, %i\n", map[y1][x1], y1, x1);
-                // printf("debug 2: %c, %i, %i\n\n", map[y2][x2], y2, x2);
                 if (y2 < 0 || x2 < 0) break;
 
                 int disty = y2 - y1, distx = x2 - x1;
@@ -55,9 +52,4 @@ int main() {
         }
     }
     printf("count: %i\n", count);
-
-    return 0;
-    for (int i = 0; i < SIZE; i++) {
-        printf("%.*s\n", SIZE, map[i]);
-    }
 }
