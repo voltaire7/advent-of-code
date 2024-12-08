b8f4dd25951dc21a14f02ea16531b60236ace940
Ioan Mark Neaga <ioanneaga@outlook.com>
2024-12-08 04:39:37 -0500

    [2024/c/8-*] Updated find to be nicer to look at.

--- 2024/c/8-1.c
+++ 2024/c/8-1.c
@@ -7,17 +7,17 @@
 char map  [SIZE][SIZE] = {};
 bool bools[SIZE][SIZE] = {};
 
-void find(int *y, int *x) {
+bool find(int *y, int *x) {
     char c = map[*y][*x];
     for (int i = *y; i < SIZE; i++) {
         for (int j = i > *y ? 0 : *x + 1; j < SIZE; j++) {
             if (map[i][j] == c) {
                 *y = i, *x = j;
-                return;
+                return true;
             }
         }
     }
-    *y = -1, *x = -1;
+    return false;
 }
 
 int main() {
@@ -29,7 +29,7 @@ int main() {
             if (map[y1][x1] == '.') continue;
             char c = map[y1][x1];
             int y2 = y1, x2 = x1;
-            while (find(&y2, &x2), y2 >= 0 && x2 >= 0) {
+            while (find(&y2, &x2)) {
                 int disty = y2 - y1, distx = x2 - x1;
                 if (y1-disty >= 0 && y1-disty < SIZE && x1-distx >= 0 && x1-distx < SIZE) bools[y1-disty][x1-distx] = true;
                 if (y2+disty >= 0 && y2+disty < SIZE && x2+distx >= 0 && x2+distx < SIZE) bools[y2+disty][x2+distx] = true;
